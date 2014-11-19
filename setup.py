49e52ac349491796939ea1873789a16e1996d7f9
mwielgoszewski <marcin.wielgoszewski@gmail.com>
2014-11-19 10:53:48 -0500

    Begin making this a pypi distributable

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='vivisect',
+    author='',
+    author_email='',
+    version='',
+    url='https://github.com/vivisect/vivisect',
+    py_modules=[''],
+    packages=find_packages(),
+    scripts=['vdbbin', 'vivbin',],
+    description='',
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python'
+        'Topic :: Security',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development :: Disassemblers',
+    ]
+)
