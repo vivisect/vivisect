d73c26060c851f9feb50f5188f1a862c7e4af949
atlas <atlas@r4780y.com>
2015-01-27 22:53:20 -0500

    fuck me, the same bug exists in amd64 too.  this cleared up a lot of arg repr stuff there too.

--- vivisect/analysis/amd64/emulation.py
+++ vivisect/analysis/amd64/emulation.py
@@ -139,13 +139,12 @@ def analyzeFunction(vw, fva):
     cc = emu.getCallingConvention(callconv)
     stcount = cc.getNumStackArgs(emu, argc)
     stackidx = argc - stcount
+    baseoff = cc.getStackArgOffset(emu, argc)
 
     # Register our stack args as function locals
-    for i in xrange( argc ):
-        if i < stackidx:
-            continue
+    for i in xrange( stcount ):
 
-        vw.setFunctionLocal(fva, 4 + ( i * 8 ), LSYM_FARG, i)
+        vw.setFunctionLocal(fva, baseoff + ( i * 8 ), LSYM_FARG, i+stackidx)
 
     emumon.addAnalysisResults(vw, emu)
 
