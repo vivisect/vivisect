ff7be2369bf85000a84e915227d321d753aef65b
invisigoth <invisigoth@kenshoto.com>
2015-02-21 11:09:06 -0500

    tweaks to thishost docs etc

--- vivisect/lib/thishost.py
+++ vivisect/lib/thishost.py
@@ -47,12 +47,33 @@ hostinfo = getHostInfo()
 locals().update( hostinfo )
 
 def require(**reqinfo):
+    '''
+    Require a given set of hostinfo values.
+
+    Example:
+        import vivisect.lib.thishost as v_thishost
+
+        # this will raise an exception on platforms other
+        # than linux.
+
+        v_thishost.require(platform='linux')
+
+    '''
     for key,val in reqinfo.items():
         cur = hostinfo.get(key)
         if cur != val:
             raise Exception('thishost: %s=%s required (is: %s=%s)' % (key,val,key,cur))
 
 def check(**reqinfo):
+    '''
+    Check a set of values from the current hostinfo.
+
+    Example:
+        import vivisect.lib.thishost as v_thishost
+
+        if v_thishost.check(platform='windows',arch='arm'):
+            print('on a windows arm device!')
+    '''
     for key,val in reqinfo.items():
         cur = hostinfo.get(key)
         if cur != val:
