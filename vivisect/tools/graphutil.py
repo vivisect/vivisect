f3a43beb741c472bb313832c0106b8fdcd8e68b2
atlas <atlas@r4780y.com>
2015-02-27 14:24:26 -0500

    reduceGraph() helper function for graph routing/trimming

--- vivisect/tools/graphutil.py
+++ vivisect/tools/graphutil.py
@@ -629,7 +629,16 @@ def clearGraphRouting(graph):
     for nid, ninfo in graph.getNodes():
         graph.getNodeProps(nid)['up'  ] = False
         graph.getNodeProps(nid)['down'] = False
-    
+
+def reduceGraph(graph, props=['up','down']):
+    '''
+    trims all nodes that don't have all the props in the props list
+    '''
+    for node in graph.getNodes():
+        for prop in props:
+            if node[1].get(prop) == None:
+                graph.delNode(node)
+                break
 
 def preRouteGraphUp(graph, tova, loops=True):
     '''
