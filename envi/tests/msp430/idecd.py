4d805613ad4c508f70885a006dde30c51002e7db
Roi Martin <jroi.martin@gmail.com>
2016-01-02 18:07:59 +0100

    Add missing test cases for DECD

--- envi/tests/msp430/idecd.py
+++ envi/tests/msp430/idecd.py
@@ -18,7 +18,12 @@ checks = [
         { 'regs': [(REG_R15, 0x7fff)], 'flags': [(SR_N, 0), (SR_Z, 0), (SR_C, 0), (SR_V, 1)], 'code': "2f83", 'data': "" }
     ),
     (
-        'DECD r15 (destination carry + negative)',
+        'DECD r15 (destination 0x0 + carry + negative)',
+        { 'regs': [(REG_R15, 0x0)], 'flags': [(SR_N, 0), (SR_Z, 0), (SR_C, 0), (SR_V, 0)], 'code': "2f83", 'data': "" },
+        { 'regs': [(REG_R15, 0xfffe)], 'flags': [(SR_N, 1), (SR_Z, 0), (SR_C, 1), (SR_V, 0)], 'code': "2f83", 'data': "" }
+    ),
+    (
+        'DECD r15 (destination 0x1 + carry + negative)',
         { 'regs': [(REG_R15, 0x1)], 'flags': [(SR_N, 0), (SR_Z, 0), (SR_C, 0), (SR_V, 0)], 'code': "2f83", 'data': "" },
         { 'regs': [(REG_R15, 0xffff)], 'flags': [(SR_N, 1), (SR_Z, 0), (SR_C, 1), (SR_V, 0)], 'code': "2f83", 'data': "" }
     ),
@@ -40,7 +45,12 @@ checks = [
         { 'regs': [(REG_R15, 0x7f)], 'flags': [(SR_N, 0), (SR_Z, 0), (SR_C, 0), (SR_V, 1)], 'code': "6f83", 'data': "" }
     ),
     (
-        'DECD.b r15 (destination carry + negative)',
+        'DECD.b r15 (destination 0x0 + carry + negative)',
+        { 'regs': [(REG_R15, 0xff00)], 'flags': [(SR_N, 0), (SR_Z, 0), (SR_C, 0), (SR_V, 0)], 'code': "6f83", 'data': "" },
+        { 'regs': [(REG_R15, 0xfe)], 'flags': [(SR_N, 1), (SR_Z, 0), (SR_C, 1), (SR_V, 0)], 'code': "6f83", 'data': "" }
+    ),
+    (
+        'DECD.b r15 (destination 0x1 + carry + negative)',
         { 'regs': [(REG_R15, 0xff01)], 'flags': [(SR_N, 0), (SR_Z, 0), (SR_C, 0), (SR_V, 0)], 'code': "6f83", 'data': "" },
         { 'regs': [(REG_R15, 0xff)], 'flags': [(SR_N, 1), (SR_Z, 0), (SR_C, 1), (SR_V, 0)], 'code': "6f83", 'data': "" }
     ),
