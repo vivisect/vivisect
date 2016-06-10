b4c1f67f829e99d5b2af5277c129a86a44939a9b
atlas <atlas@r4780y.com>
2016-06-09 22:54:36 -0400

    enhanced vasets, allowing for more flexibility in representation of the data.

--- vivisect/const.py
+++ vivisect/const.py
@@ -175,6 +175,8 @@ LSYM_FARG   = 1 # syminfo is an argument index
 VASET_ADDRESS   = 0
 VASET_INTEGER   = 1
 VASET_STRING    = 2
+VASET_HEXTUP    = 3
+VASET_COMPLEX   = 4
 
 # Symboliks effect types
 EFFTYPE_DEBUG        = 0
