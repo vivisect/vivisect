e9501c9f40937a9a032b641e468e2abe4290dc1b
redheadedrod <redheadedrod@hotmail.com>
2016-06-23 12:35:23 -0400

    New archMask setup.

--- envi/archs/arm/__init__.py
+++ envi/archs/arm/__init__.py
@@ -10,11 +10,11 @@ from envi.archs.arm.disasm import *
 class ArmModule(envi.ArchitectureModule):
 
     def __init__(self, name='ARMv7A'):
-        self.archVersion = name
         import envi.archs.thumb16.disasm as eatd
         envi.ArchitectureModule.__init__(self, name, maxinst=4)
         self._arch_reg = self.archGetRegCtx()
         self._arch_dis = ArmDisasm()
+        self._arch_dis.setArchMask(name)
         self._arch_thumb_dis = eatd.ThumbDisasm()
 
     def archGetRegCtx(self):
@@ -51,13 +51,13 @@ class ThumbModule(envi.ArchitectureModule):
     This architecture module will *not* shift to ARM mode.  Evar.
     '''
 
-    def __init__(self, name='ARMv7A'):
-        self.archVersion = name
+    def __init__(self, name='ARMv7'):
         import envi.archs.thumb16.disasm as eatd
         envi.ArchitectureModule.__init__(self, name, maxinst=4)
         self._arch_reg = self.archGetRegCtx()
         self._arch_dis = eatd.ThumbDisasm(doModeSwitch=False)
-
+        #armVersion mask should be set here if needed
+        
     def archGetRegCtx(self):
         return ArmRegisterContext()
 
