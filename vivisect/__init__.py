cdd48a2bbe28953d9115af333f08e778ef3c591a
atlas <atlas@r4780y.com>
2018-03-10 18:32:32 -0500

    cleanup

--- vivisect/__init__.py
+++ vivisect/__init__.py
@@ -667,7 +667,18 @@ class VivWorkspace(e_mem.MemoryObject, viv_base.VivWorkspaceCore):
         return stats
 
     def printDiscoveredStats(self):
-        disc, undisc, numXrefs, numLocs, numFuncs, numBlocks, numOps, numUnis, numStrings, numNumbers, numPointers, numVtables = self.getDiscoveredInfo()
+        ( disc, 
+            undisc, 
+            numXrefs, 
+            numLocs, 
+            numFuncs, 
+            numBlocks, 
+            numOps, 
+            numUnis, 
+            numStrings, 
+            numNumbers, 
+            numPointers, 
+            numVtables ) = self.getDiscoveredInfo()
         self.vprint("Percentage of discovered executable surface area: %.1f%% (%s / %s)" % (disc*100.0/(disc+undisc), disc, disc+undisc))
         self.vprint("   Xrefs/Blocks/Funcs:                             (%s / %s / %s)" % (numXrefs, numBlocks, numFuncs))
         self.vprint("   Locs,  Ops/Strings/Unicode/Nums/Ptrs/Vtables:   (%s:  %s / %s / %s / %s / %s / %s)" % (numLocs, numOps, numStrings, numUnis, numNumbers, numPointers, numVtables))
