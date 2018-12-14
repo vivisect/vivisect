ce1e9d0b8f9a4d86cdc11fb38025f6b3e2b7a85a
atlas <atlas@r4780y.com>
2018-12-14 14:23:54 -0500

    unify the ThumbModule.

--- envi/archs/thumb16/__init__.py
+++ envi/archs/thumb16/__init__.py
@@ -3,17 +3,8 @@ from envi.archs.arm import *
 
 import disasm as th_disasm
 
-class Thumb16Module(ArmModule):
+class Thumb16Module(ThumbModule):
 
     def __init__(self):
-        ArmModule.__init__(self, name='thumb16')
-        self._arch_dis = self._arch_thumb_dis
-
-
-class ThumbModule(ArmModule):
-
-    def __init__(self):
-        ArmModule.__init__(self, name='thumb')
-        self._arch_dis = self._arch_thumb_dis
-
+        ThumbModule.__init__(self, name='thumb16')
 
