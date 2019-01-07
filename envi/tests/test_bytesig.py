00810b8d924f9b7123fa90f50c9e2373ddc8fcb3
rakuyo <james.gross@vertex.link>
2019-01-07 12:10:40 -0500

    Names are hard. Also handle case where length of bytes to match is shorter than the final signature hit (and add test for it).

--- envi/tests/test_bytesig.py
+++ envi/tests/test_bytesig.py
@@ -8,12 +8,19 @@ class SignatureTests(unittest.TestCase):
         signature_base = '\x55\xe9\xd8\x01\xfe\xff\x32\x77\x89\x4f\x55'
         sigtree = envi.bytesig.SignatureTree()
         sigtree.addSignature(signature_base)
+
+        try:
+            sigtree.getSignature('\x55\xe9')
+        except IndexError, e:
+            self.fail("%s" % (traceback.format_exc(),))
+
         try:
             sigtree.addSignature(signature_base[:7], val=signature_base[:7])
             sigtree.addSignature(signature_base[:4], val=signature_base[:4])
             sigtree.addSignature(signature_base + '\xfe\x38', val=signature_base + '\xfe\x38')
         except IndexError, e:
             self.fail("%s" % (traceback.format_exc(),))
+
         self.assertTrue(sigtree.getSignature('\x55\xe9\xd8\x01\xfe\xff\x32\x00\x99\x36\x5f\x21\xfd') == signature_base[:7])
         self.assertTrue(sigtree.getSignature('\x55\xe9\xd8\x01\xfe\xff\x32') == signature_base[:7])
         self.assertTrue(sigtree.getSignature('\x55\xe9\xd8\x01\xfe\x00') == signature_base[:4])
