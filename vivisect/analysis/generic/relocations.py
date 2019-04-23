138132769e0d7d235c9b203bc22c3ecf26aaa87b
rakuyo <james.gross@vertex.link>
2019-04-23 14:51:02 -0400

    fix access violation in 64 bit binaries

--- vivisect/analysis/generic/relocations.py
+++ vivisect/analysis/generic/relocations.py
@@ -8,11 +8,11 @@ import vivisect
 
 def analyze(vw):
     for fname, vaoff, rtype, data in vw.getRelocations():
+        imgbase = vw.getFileMeta(fname, 'imagebase')
         if rtype == vivisect.RTYPE_BASERELOC and not vw.isLocation(vaoff):
-            vw.makePointer(vaoff, follow=True)
+            vw.makePointer(imgbase + vaoff, follow=True)
 
         elif rtype == vivisect.RTYPE_BASEOFF:
-            imgbase = vw.getFileMeta(fname, 'imagebase')
             va = imgbase + vaoff
             if not vw.isLocation(va):
                 vw.makePointer(va, follow=True)
