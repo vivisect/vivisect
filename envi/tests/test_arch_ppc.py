17c571ea2815f70fc9b061a16a5041bc48cb5b09
atlas <atlas@r4780y.com>
2019-05-13 12:55:53 -0400

    minor updates to ppc unit tests, which will be dramatically updated with @sprout42's updates.

--- envi/tests/test_arch_ppc.py
+++ envi/tests/test_arch_ppc.py
@@ -4,14 +4,24 @@ import envi.archs.ppc
 import unittest
 
 class PpcInstructionSet(unittest.TestCase):
+    def getVivEnv(self, arch='ppc'):
+        vw = vivisect.VivWorkspace()
+        vw.setMeta("Architecture", arch)
+        vw.addMemoryMap(0, 7, 'firmware', '\xff' * 16384*1024)
+        vw.addMemoryMap(0xbfb00000, 7, 'firmware', '\xfe' * 16384*1024)
+
+        emu = vw.getEmulator()
+        emu.setMeta('forrealz', True)
+        emu.logread = emu.logwrite = True
+
+        sctx = vs_anal.getSymbolikAnalysisContext(vw)
+        return vw, emu, sctx
+
     def test_envi_ppcvle_disasm(self):
         test_pass = 0
 
-        vw = vivisect.VivWorkspace()
-        vw.setMeta("Architecture", "vle")
-        va = 0x00000000
-        vw.addMemoryMap(va, 7, 'firmware', '\xff' * 16384*1024)
-
+        vw, emu, sctx = self.getVivEnv('vle')
+        
         import ppc_vle_instructions
         for test_bytes, result_instr in ppc_vle_instructions.instructions:
             op = vw.arch.archParseOpcode(test_bytes.decode('hex'), 0, va)
@@ -25,10 +35,7 @@ class PpcInstructionSet(unittest.TestCase):
     def test_envi_ppc64_disasm(self):
         test_pass = 0
 
-        vw = vivisect.VivWorkspace()
-        vw.setMeta("Architecture", "ppc")
-        va = 0x00000000
-        vw.addMemoryMap(va, 7, 'firmware', '\xff' * 16384*1024)
+        vw, emu, sctx = self.getVivEnv('ppc')
 
         import ppc64_instructions
         for test_bytes, result_instr in ppc64_instructions.instructions:
@@ -39,3 +46,19 @@ class PpcInstructionSet(unittest.TestCase):
             self.assertEqual(result_instr, op_str, '{}: {} != {}'.format(test_bytes, result_instr, op_str))
 
         self.assertEqual(test_pass, len(ppc64_instructions.instructions))
+
+    def test_MASK_and_ROTL32(self):
+        import envi.archs.ppc.emu as eape
+        import vivisect.symboliks.archs.ppc as vsap
+
+        for x in range(64):
+            for y in range(64):
+                #mask = 
+                emumask = eape.MASK(x, y)
+
+                symmask = vsap.MASK(vsap.Const(x, 8), vsap.Const(y, 8))
+                #print hex(emumask), repr(symmask), symmask
+
+
+                if emumask != symmask.solve():
+                    print "FAIL!!!!"
