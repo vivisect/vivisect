474e026f751366bb909facd7136acd45c84c28e0
atlas <atlas@r4780y.com>
2019-07-15 15:32:20 -0400

    ELF unit tests. these will need changing as we add new functionality to allow ELF Dynamics-loading (which will change the results for the QNX and OpenBSD bins)

---
+++ vivisect/tests/openbsd_amd64_ls_data.py
@@ -0,0 +1,13 @@
+ls_amd64_data = {
+    'imports' : [
+    ],
+    'exports' : [
+        (0x400230, 0, '__entry', 'ls'),
+    ],
+    'relocs' : [
+    ],
+    'pltgot' : [
+    ],
+}
+
+
---
+++ vivisect/tests/qnx_arm_ksh_data.py
@@ -0,0 +1,13 @@
+ksh_data = {
+    'imports' : [
+    ],
+    'exports' : [
+        (0x104114, 0, '__entry', 'ksh'),
+    ],
+    'relocs' : [
+    ],
+    'pltgot' : [
+    ],
+}
+
+
