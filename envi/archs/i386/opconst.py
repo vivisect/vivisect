bc6f973255b4ee4406801c86ae83f619810904a6
rakuyo <james.gross@vertex.link>
2020-01-23 13:15:39 -0500

    fmadd instructions

--- envi/archs/i386/opconst.py
+++ envi/archs/i386/opconst.py
@@ -242,6 +242,7 @@ cpu_PENTMMX = 0x00008000
 cpu_PENTIUM2 = 0x00009000
 cpu_AMD64 = 0x0000a000
 cpu_AESNI = 0x0000b000
+cpu_AVX   = 0x0000c000
 
 #eventually, change this for your own codes
 #ADDEXP_SCALE_OFFSET= 0 
