0f27acea10dcb404b778f81b382f1b859684fdf1
rakuyo <james.gross@vertex.link>
2020-07-16 23:38:57 -0400

    well, it installs the packages and we can use it as a library. but the ui is hosed until we move to py3, and it smashes PE/Elf/envi/etc ontop of the global package namespace. so there's that too

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='vivisect',
+    author='The Vivisect Cats',
+    author_email='',
+    version='0.1.0',
+    url='https://github.com/vivisect/vivisect',
+    packages=find_packages(),
+    zip_safe=False,
+    description='Pure python disassembler, debugger, emulator, and static analysis framework',
+    entry_points={
+        'console_scripts': [
+            'vivbin=vivisect.vivbin:main',
+            'vdbbin=vdbbin.vdbbin:main',
+        ]
+    },
+    install_requires=[
+        'pyasn1==0.4.5',
+        'pyasn1-modules==0.2.4',
+        'cxxfilt==0.2.1',
+        'msgpack==1.0.0',
+        'pycparser==2.20',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Topic :: Security',
+        'Topic :: Software Development :: Disassemblers',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: System :: Emulators',
+    ]
+)
