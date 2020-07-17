2f481e40d52602b4b541ce81990222305e2082e0
atlas0fd00m <atlas@r4780y.com>
2020-07-17 19:19:29 -0400

    bugfix: reprLocEntry() instead of reprLocation()

--- vivisect/analysis/generic/pointers.py
+++ vivisect/analysis/generic/pointers.py
@@ -73,7 +73,7 @@ def analyze(vw):
 
             loc = vw.getLocation(ptr)
             if loc is not None and loc[L_LTYPE] != LOC_POINTER:
-                logger.info('skipping naming of 0x%x (no longer a pointer: %s)', ptr, vw.reprLocation(ptr))
+                logger.info('skipping naming of 0x%x (no longer a pointer: %s)', ptr, vw.reprLocEntry(ptr))
                 continue
 
             tgtname = vw.getName(tgt)
