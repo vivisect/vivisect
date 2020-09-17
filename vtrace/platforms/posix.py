7358fce38d4c7d1e184e97c270c814ff269571ee
rakuyo <james.gross@vertex.link>
2020-09-16 22:49:56 -0400

    closer

--- vtrace/platforms/posix.py
+++ vtrace/platforms/posix.py
@@ -57,7 +57,7 @@ class PosixMixin:
 
     def handleAttach(self):
         self.fireNotifiers(vtrace.NOTIFY_ATTACH)
-        self._findLibraryMaps('\x7fELF', always=True)
+        self._findLibraryMaps(b'\x7fELF', always=True)
         self._simpleCreateThreads()
         # We'll emulate windows here and send an additional
         # break after our library load events to make things easy
