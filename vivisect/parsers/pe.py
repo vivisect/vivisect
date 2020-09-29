4ca9f0559c11b8856ad2f868348ed23d583dba71
rakuyo <james.gross@vertex.link>
2020-09-29 01:41:10 -0400

    little more stricter (since exe imgs can have it set to it)

--- vivisect/parsers/pe.py
+++ vivisect/parsers/pe.py
@@ -188,8 +188,8 @@ def loadPeIntoWorkspace(vw, pe, filename=None, baseaddr=None):
     ifhdr_va = baseaddr + magicaddr + 4
     ifstruct = vw.makeStructure(ifhdr_va, "pe.IMAGE_FILE_HEADER")
     ohstruct = vw.makeStructure(ifhdr_va + len(ifstruct), "pe.IMAGE_OPTIONAL_HEADER")
-    isdll = ifstruct.Characteristics & PE.IMAGE_FILE_DLL == PE.IMAGE_FILE_DLL
-    nxcompat = isdll and (ohstruct.DllCharacteristics & PE.IMAGE_DLLCHARACTERISTICS_NX_COMPAT == PE.IMAGE_DLLCHARACTERISTICS_NX_COMPAT)
+    isdll = ifstruct.Characteristics & PE.IMAGE_FILE_DLL
+    nxcompat = ohstruct.DllCharacteristics & PE.IMAGE_DLLCHARACTERISTICS_NX_COMPAT
 
     # get resource data directory
     ddir = pe.getDataDirectory(PE.IMAGE_DIRECTORY_ENTRY_RESOURCE)
@@ -230,7 +230,7 @@ def loadPeIntoWorkspace(vw, pe, filename=None, baseaddr=None):
 
             # If it's for an older system, just about anything
             # is executable...
-            if not isdll and not nxcompat:
+            if not nxcompat:
                 if not vw.config.viv.parsers.pe.nx and subsys_majver < 6 and not isrsrc:
                     mapflags |= e_mem.MM_EXEC
 
@@ -260,8 +260,9 @@ def loadPeIntoWorkspace(vw, pe, filename=None, baseaddr=None):
         if secrva <= entryrva and entryrva < secrvamax:
             mapflags |= e_mem.MM_EXEC
 
-        if not vw.config.viv.parsers.pe.nx and subsys_majver < 6 and mapflags & e_mem.MM_READ:
-            mapflags |= e_mem.MM_EXEC
+        if not nxcompat:
+            if not vw.config.viv.parsers.pe.nx and subsys_majver < 6 and mapflags & e_mem.MM_READ:
+                mapflags |= e_mem.MM_EXEC
 
         if sec.VirtualSize == 0 or sec.SizeOfRawData == 0:
             if idx+1 >= len(pe.sections):
