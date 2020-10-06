210a1cccbcc234d09cce0fcd5d3d4106af476c67
rakuyo <james.gross@vertex.link>
2020-10-05 21:56:58 -0400

    git shenanigans

--- PE/carve.py
+++ PE/carve.py
@@ -44,10 +44,6 @@ def carve(pbytes, offset=0):
         if nextres != -1:
             todo.append((nextres, mzx, pex, i))
 
-        nextres = pbytes.find(mzx, off+1)
-        if nextres != -1:
-            todo.append( (nextres, mzx, pex, i) )
-
         peoff = off + newoff
         if pblen < (peoff + 2):
             continue
