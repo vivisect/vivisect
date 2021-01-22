8ec9fa3a55f14dcd056b7066f97b82879b796afe
atlas <atlas@r4780y.com>
2021-01-22 08:48:27 -0500

    vivbin really wants to read in more than one option at a time!

--- vivisect/vivbin.py
+++ vivisect/vivbin.py
@@ -36,7 +36,9 @@ def main():
                         help='Do *not* start the gui, just load, analyze and save')
     parser.add_argument('-C', '--cprofile', dest='cprof', default=False, action='store_true',
                         help='Output vivisect performace profiling (cProfile) info')
-    parser.add_argument('-O', '--option', dest='option', default=None, action='store',
+    parser.add_argument('-E', '--entrypoint', dest='addEntryPoints', default=[], action='append',
+                        help='Add Entry Point for bulk analysis (can have multiple "-E <addr>" args')
+    parser.add_argument('-O', '--option', dest='option', default=None, action='append',
                         help='<secname>.<optname>=<optval> (optval must be json syntax)')
     parser.add_argument('-p', '--parser', dest='parsemod', default=None, action='store',
                         help='Manually specify the parser module (pe/elf/blob/...)')
@@ -60,23 +62,24 @@ def main():
     logger.setLevel(loglevels[vw.verbose])
 
     if args.option is not None:
-        if args.option in ('-h', '?'):
-            logger.critical(vw.config.reprConfigPaths())
-            sys.exit(-1)
+        for option in args.option:
+            if option in ('-h', '?'):
+                logger.critical(vw.config.reprConfigPaths())
+                sys.exit(-1)
 
-        try:
-            vw.config.parseConfigOption(args.option)
-        except e_config.ConfigNoAssignment as e:
-            logger.critical(vw.config.reprConfigPaths() + "\n")
-            logger.critical(e)
-            logger.critical("syntax: \t-O <secname>.<optname>=<optval> (optval must be json syntax)")
-            sys.exit(-1)
+            try:
+                vw.config.parseConfigOption(option)
+            except e_config.ConfigNoAssignment as e:
+                logger.critical(vw.config.reprConfigPaths() + "\n")
+                logger.critical(e)
+                logger.critical("syntax: \t-O <secname>.<optname>=<optval> (optval must be json syntax)")
+                sys.exit(-1)
 
-        except Exception as e:
-            logger.critical(vw.config.reprConfigPaths())
-            logger.critical("With entry: %s", args.option)
-            logger.critical(e)
-            sys.exit(-1)
+            except Exception as e:
+                logger.critical(vw.config.reprConfigPaths())
+                logger.critical("With entry: %s", option)
+                logger.critical(e)
+                sys.exit(-1)
 
     if args.storage_name is not None:
         vw.setMeta("StorageModule", args.storage_name)
@@ -103,6 +106,14 @@ def main():
             logger.info('Loaded (%.4f sec) %s', (end - start), fname)
 
     if args.bulk:
+        for entryva in args.addEntryPoints:
+            try:
+                vw.vprint("Adding Entry Point: %s: " % entryva)
+                eva = int(entryva, 0)
+                vw.setVaSetRow('EntryPoints', (eva,))
+            except Exception, e:
+                vw.vprint("Failure: %r" % e)
+
         if args.doanalyze:
             if args.cprof:
                 cProfile.run("vw.analyze()")
