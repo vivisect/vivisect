806257efc56f2e799ecc8a0c817b65422b520d75
rakuyo <james.gross@vertex.link>
2021-03-03 23:33:02 -0500

    swapper done

--- vivisect/storage/tools/convert.py
+++ vivisect/storage/tools/convert.py
@@ -1,19 +1,57 @@
+import os
+import sys
 import argparse
 
+import vivisect
+import vivisect.parsers as v_parsers
 import vivisect.storage as v_storage
 
-def setup():
-    ap = argparse.ArgumentParser('')
+storemap = {
+    'viv': 'vivisect.storage.basicfile',
+    'mpviv': 'vivisect.storage.mpfile',
+}
 
-    ap.add_argument('')
-    ap.add_argument('')
-    ap.add_argument('')
+
+def setup():
+    ap = argparse.ArgumentParser('Convert from one workspace format to another')
+
+    ap.add_argument('old', help='Path to older workspace')
+    ap.add_argument('--name', '-n', help='Name for the new workspace')
 
     return ap
 
 
+def convert(old, newname):
+    oldfmt = v_parsers.guessFormatFilename(old)
+    vw = vivisect.VivWorkspace()
+    if oldfmt not in storemap:
+        print('Refusing to handle format %s, this is for workspace files only!')
+        return -1
+
+    newfmt = 'mpviv'
+    if oldfmt == 'mpviv':
+        newfmt = 'viv'
+
+    if not newname:
+        newname = '%s.%s' % (old, newfmt)
+
+    oldstor = storemap[oldfmt]
+    newstor = storemap[newfmt]
+
+    vw.setMeta('StorageModule', oldstor)
+    vw.loadWorkspace(old)
+
+    vw.setMeta('StorageModule', newstor)
+    vw.setMeta('StorageName', newname)
+    vw.saveWorkspace()
+
+
 def main(argv):
     opts = setup().parse_args(argv)
+
+    old = os.path.abspath(opts.old)
+
+    convert(old, opts.name)
 
 
 if __name__ == '__main__':
