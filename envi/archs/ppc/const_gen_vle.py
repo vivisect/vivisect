326f24a15eecb5bff73c836a13a9d81928bcae11
atlas <atlas@r4780y.com>
2021-03-25 10:18:33 -0400

    the one that got away in the merge...

---
+++ envi/archs/ppc/const_gen_vle.py
@@ -0,0 +1,39 @@
+
+
+# VLE opcode types
+# FIXME: generate automatically like mnems.py
+mnems = (
+    'ill',
+
+    'add',
+    'sub',
+    'mul',
+    'div',
+    'shr',
+    'shl',
+    'ror',
+
+    'and',
+    'or',
+    'xor',
+    'nor',
+    'not',
+
+    'io',
+    'load',
+    'store',
+    'mov',
+
+    'cmp',
+    'jmp',
+    'cjmp',
+    'call',
+    'ccall',
+    'rjmp',
+    'rcall',
+    'ret',
+
+    'sync',
+    'swi',
+    'trap',
+)
