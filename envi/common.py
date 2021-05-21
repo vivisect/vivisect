8ec330dde1ee81e097f26ab8a16b091d3d213df7
atlas <atlas@r4780y.com>
2021-05-21 14:31:01 -0400

    BUGFIX: CodeFlow was *not* operating depth-first!  since before the 2014 cutover to Github! and all the things that happened because of that (unittests)

--- envi/common.py
+++ envi/common.py
@@ -4,6 +4,7 @@ LOG_FORMAT = '%(asctime)s:%(levelname)s:%(name)s: %(message)s' \
              '[%(filename)s:%(funcName)s:%(lineno)s:%(threadName)s]'
 
 EMULOG = 9
+SHITE = 8
 
 LOG_LEVELS = (
     logging.CRITICAL,
@@ -12,6 +13,7 @@ LOG_LEVELS = (
     logging.INFO,
     logging.DEBUG,
     EMULOG,
+    SHITE,
 )
 
 def initLogging(logger, level=None, fmt=LOG_FORMAT):
@@ -26,6 +28,7 @@ def initLogging(logger, level=None, fmt=LOG_FORMAT):
 
     # Setup logging
     logging.addLevelName(EMULOG, 'EMULOG')
+    logging.addLevelName(SHITE, 'SHITE')
     logging.basicConfig(level=level, format=fmt)
 
     # Set log level for current logger
