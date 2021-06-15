854f9168502d1ee728860873d9dc7e1946480ecf
atlas <atlas@r4780y.com>
2021-06-15 15:12:42 -0400

    update unittests

--- vtrace/tests/tests_envitools.py
+++ vtrace/tests/tests_envitools.py
@@ -1,13 +1,24 @@
 import vtrace
 import vtrace.envitools
+import vtrace.util
 
 
 class EnvitoolsTests(vtrace.tests.VtraceProcessTest):
     def test_emulatorFromTrace(self):
-        emu = vtrace.envitools.emuFromTrace(self.trace)
+        emu = vtrace.util.emuFromTrace(self.trace)
 
         ctx = self.trace.getRegisterContext()
         for rname, idx in ctx.getRegisterNameIndexes():
             self.assertEqual(self.trace.getRegister(idx), emu.getRegister(idx))
 
         emu.stepi()
+
+    def test_workspaceFromTrace(self):
+        vw = vtrace.util.vwFromTrace(self.trace, collapse=False)
+        vwmaps = list(vw.getMemoryMaps())
+
+        for mva, msize, mperms, mfname in self.trace.getMemoryMaps():
+
+            # test that the file mappings exist for each (which means segments have to be correct)
+            filename = vw.getFileByVa(mva)
+            self.assertIn(filename, mfname)
