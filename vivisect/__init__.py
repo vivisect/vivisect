587a9934d3388f12abc3f183c5f3193e031d5a48
atlas <atlas@r4780y.com>
2021-08-19 16:08:36 -0400

    fix ascii string length calculations when the string terminates at the end of a memory map.

--- vivisect/__init__.py
+++ vivisect/__init__.py
@@ -2271,7 +2271,7 @@ class VivWorkspace(e_mem.MemoryObject, viv_base.VivWorkspaceCore):
         offset, bytez = self.getByteDef(va)
         foff = bytez.find(b'\x00', offset)
         if foff == -1:
-            return foff
+            return len(bytez) - offset
         return (foff - offset) + 1
 
     def uniStringSize(self, va):
