10b602c6a909241c1463a662ca7d792b23faf92c
atlas <atlas@r4780y.com>
2021-10-15 07:40:05 -0400

    update to identify FreeBSD ELF files (OS matters on some of these vtrace and emu changes)

--- Elf/__init__.py
+++ Elf/__init__.py
@@ -879,6 +879,14 @@ class Elf(vs_elf.Elf32, vs_elf.Elf64):
                 desc0 = int(note.desc[0])
                 return osnotes.get(desc0, 'unknown')
 
+            #if note.name == b'01.01\x00\x00\x00':
+            #    return 'freebsd'
+
+        if self.getSection('.comment'):
+            sec = self.getSection('.comment')
+            if b'FreeBSD' in self.readAtOffset(sec.sh_offset, sec.sh_size):
+                return 'freebsd'
+
         if self.getSection('QNX_info'):
             return 'qnx'
 
