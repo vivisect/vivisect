79674a38e620c026ae33b39b77f3dada7dbaeb6d
atlas <atlas@r4780y.com>
2021-12-29 17:38:18 -0500

    is this something we've missed for two years?

--- vivisect/base.py
+++ vivisect/base.py
@@ -637,6 +637,17 @@ class VivWorkspaceCore(viv_impapi.ImportApi):
     #def _loadImportApi(self, apidict):
         #self._imp_api.update( apidict )
 
+    def getEndian(self):
+        return self.bigend
+
+    def _doSetEndian(self, endian):
+        self.bigend = endian
+        for arch in self.imem_archs:
+            arch.setEndian(self.bigend)
+
+        if self.arch is not None:
+            self.arch.setEndian(self.bigend)
+
 
 #################################################################
 #
