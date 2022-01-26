c70c86bddc3e9b2e0654c033b51616c59fd60de0
atlas <atlas@r4780y.com>
2022-01-26 08:55:56 -0500

    bugfix in unittest

--- vivisect/tests/helpers.py
+++ vivisect/tests/helpers.py
@@ -49,7 +49,7 @@ def getTestWorkspace(*paths):
     testdir = os.path.abspath(testdir)
     fpath = os.path.join(testdir, *paths)
     vw = v_cli.VivCli()
-    vw.config.analysis.symswitchcase.timeout_secs = 30
+    vw.config.viv.analysis.symswitchcase.timeout_secs = 30
     vw.loadFromFile(fpath)
     vw.analyze()
     return vw
