5f0be086a4f44da414fb8bdb7cb45cc15557b0f1
atlas <atlas@r4780y.com>
2022-05-23 17:12:44 -0400

    wrap in the VivCli's symobj when creating a new emulator from vw.getEmulator()

--- vivisect/__init__.py
+++ vivisect/__init__.py
@@ -312,6 +312,7 @@ class VivWorkspace(e_mem.MemoryObject, viv_base.VivWorkspaceCore):
 
         emu = eclass(self, **kwargs)
         emu.setEndian(self.getEndian())
+        emu.symobj = self.symobj
 
         return emu
 
