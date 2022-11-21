54c85306feae4b91fda4fe52cb93d1dadd51930d
atlas <atlas@r4780y.com>
2022-11-21 01:24:59 -0500

    remove dependency on pywin32 and winadmin

--- vtrace/platforms/win32.py
+++ vtrace/platforms/win32.py
@@ -21,9 +21,6 @@ import vtrace.archs.i386 as v_i386
 import vtrace.archs.amd64 as v_amd64
 import vtrace.platforms.base as v_base
 
-import vtrace.platforms.winadmin
-import vtrace.platforms.win_sedebug as win_sedebug
-
 import envi
 import envi.bits as e_bits
 import envi.const as e_const
@@ -37,10 +34,6 @@ from ctypes import *
 
 logger = logging.getLogger(__name__)
 platdir = os.path.dirname(__file__)
-
-# Enable SeDebugPrivilege on Windows
-win_sedebug.enable_privs()
-
 
 kernel32 = None
 dbghelp = None
