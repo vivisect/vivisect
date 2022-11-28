3ae197ddce579d208ff2eafd19d16c1d0db364ff
rakuyo <james.gross@vertex.link>
2022-11-27 23:39:33 -0500

    fix some derps

--- vivisect/impemu/platarch/linux.py
+++ vivisect/impemu/platarch/linux.py
@@ -9,6 +9,9 @@ import vivisect.impemu.platarch.amd64 as v_i_amd64
 from vivisect.impemu.emulator import imphook
 
 class Linuxi386Emulator(v_i_i386.i386WorkspaceEmulator):
+    def __init__(self, vw, **kwargs):
+        v_i_i386.i386WorkspaceEmulator.__init__(self, vw, **kwargs)
+
     def i_int(self, op):
         syscall = op.getOperValue(0)
         if syscall != 0x80:
@@ -18,7 +21,10 @@ class Linuxi386Emulator(v_i_i386.i386WorkspaceEmulator):
         if reg == 1:
             self.stopEmu()
 
-class LinuxAmd64Emulator(v_i_i386.i386WorkspaceEmulator):
+class LinuxAmd64Emulator(v_i_amd64.Amd64WorkspaceEmulator):
+    def __init__(self, vw, **kwargs):
+        v_i_amd64.Amd64WorkspaceEmulator.__init__(self, vw, **kwargs)
+
     def i_syscall(self, op):
         # TODO: there is a long list of >300 functions we'd need to deal with here
         # some of which would exit
