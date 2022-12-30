4e24cfcac316482f4e5517f838ec843e779afc48
atlas <atlas@r4780y.com>
2022-12-30 08:46:17 -0500

    final change to completely supplant PR 557

--- envi/memory.py
+++ envi/memory.py
@@ -146,12 +146,15 @@ class IMemory:
         mmap = self.getMemoryMap(va)
         if mmap is None:
             return False
+
         mapva, mapsize, mapperm, mapfile = mmap
         mapend = mapva+mapsize
         if va+size > mapend:
             return False
-        if mapperm & perm != perm:
+
+        if mapperm & perm != perm and not self._supervisor:
             return False
+
         return True
 
     def allocateMemory(self, size, perms=MM_RWX, suggestaddr=0):
@@ -578,7 +581,7 @@ class MemoryObject(IMemory):
         for mva, mmaxva, mmap, mbytes in self._map_defs:
             if mva <= va < mmaxva:
                 mva, msize, mperms, mfname = mmap
-                if not mperms & MM_READ:
+                if not (mperms & MM_READ or self._supervisor):
                     msg = "Bad Memory Read (no READ permission): %s: %s" % (hex(va), hex(size))
                     if _origva is not None:
                         msg += " (original va: %s)" % hex(_origva)
