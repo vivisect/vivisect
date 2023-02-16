ffd58f84429bd14c1aae7bfe43ed7ac77b4669a3
sprout42 <a.aaron.cornelius@gmail.com>
2023-02-16 16:47:45 -0500

    discovered more possible valid RSP values for the gdbstub server test

--- vtrace/tests/tests_gdbstub.py
+++ vtrace/tests/tests_gdbstub.py
@@ -592,11 +592,12 @@ class TestGdbClientStub(unittest.TestCase):
         if self.client._gdb_servertype == 'qemu':
             # From shell it would be: 0x40007ffbe0
             # From a subprocess spawned by python this caries between
-            # 0x40007ffa60 and 0x40007ffaa0.
+            # 0x40007ffa50 and 0x40007ffaa0.
             #
             # (this can change slightly depending on the amount of memory 
             # consumed by the GDB stub class)
-            self.assertIn(registers['rsp'], (0x40007ffa60, 0x40007ffaa0))
+            self.assertIn(registers['rsp'],
+                          (0x40007ffa50, 0x40007ffa60, 0x40007ffaa0))
 
             # CR0 registers only available over QEMU
             self.assertEqual(registers['cr0'], 0x80010001)
