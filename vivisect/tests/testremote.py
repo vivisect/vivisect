88e3fafbde36bd5a6cceb2fadf0d7939a329212c
atlas0fd00m <atlas@r4780y.com>
2024-07-02 08:25:52 -0400

    VivServer enhancements (#645)
    * vivbin -S now starts the VivServer.  -P sets the listening port.
    modifying VivServer comms to break up massive event dumps into max chunk sizes, and favor sending Memory Maps individually.
    this helps reduce timeouts while loading massive remote workspaces (working on a 500MB workspace right now).
    tweaking the timeouts and chunk sizes still.  was taking 35 minutes to load, now down to ~16minutes over Wifi.  still not happy, looking for other ways to enhance performance.  i believe there are unnecessary delays in master, so i'm looking for them.
    
    * dramatic improvements on performance.  now for a little cleanup.
    
    * some cleanup
    
    * cleaned up.  should be gtg.
    
    * updates to cover unittests and other non-huge things
    
    * kill unused variable
    
    * better documentation for VivChunkQueue
    
    * more comment
    
    * more comment
    
    * solidly avoid race condition between Arch and Platform meta's being set before _snapInAnalysisModules()
    
    * metrics/monitoring on VivServer comms performance
    
    * changes per Rakuy0.  configurable timeouts for Cobra and VivServer
    
    * Update vivisect/remote/server.py
    
    * timeo_* to timeout_* per Rakuy0 :)  makes sense to me.
    
    * Apply suggestions from code review
    
    per rakuy0
    
    * remove "block" options from getServerWorkspace.  it's no longer part of the solution.

--- vivisect/tests/testremote.py
+++ vivisect/tests/testremote.py
@@ -13,6 +13,9 @@ import vivisect.tests.helpers as helpers
 import vivisect.remote.server as v_r_server
 
 import envi.memcanvas as e_mcanvas
+import envi.common as ecmn
+logger = ecmn.logging.getLogger(__name__)
+ecmn.initLogging(logger)
 
 def runServer(name, port):
     dirn = os.path.dirname(name)
