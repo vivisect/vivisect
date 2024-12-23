e251b0be75ba775a133fccb027e86830c02cf0d8
Jonathan Snyder <2947030+jpsnyder@users.noreply.github.com>
2024-12-23 14:58:47 -0500

    Fix references being added for dynamic api calls.

--- vivisect/impemu/monitor.py
+++ vivisect/impemu/monitor.py
@@ -107,6 +107,12 @@ class AnalysisMonitor(EmulationMonitor):
             reprargs = [emu.reprVivValue(val) for val in argv]
             self.vw.setComment(va, '%s(%s)' % (callname, ','.join(reprargs)))
             cva = self.vw.vaByName(callname)
+            if not cva:
+                # Look for imports that match the call name.
+                for import_va, _, _, import_name in self.vw.getImports():
+                    if import_name == callname:
+                        cva = import_va
+                        break
             if cva:
                 self.vw.addXref(va, cva, REF_CODE, envi.BR_PROC)
 
