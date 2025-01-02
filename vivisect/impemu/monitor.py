fe9e00f07691f96dde41fa0e92c32ec4c5e00fe4
Jonathan Snyder <2947030+jpsnyder@users.noreply.github.com>
2025-01-02 15:08:51 -0500

    Performance improvement

--- vivisect/impemu/monitor.py
+++ vivisect/impemu/monitor.py
@@ -103,13 +103,14 @@ class AnalysisMonitor(EmulationMonitor):
 
                 vw.guessDataPointer(val, tsize)
 
+        imports = self.vw.getImports()
         for va, callname, argv in self.callcomments:
             reprargs = [emu.reprVivValue(val) for val in argv]
             self.vw.setComment(va, '%s(%s)' % (callname, ','.join(reprargs)))
             cva = self.vw.vaByName(callname)
             if not cva:
                 # Look for imports that match the call name.
-                for import_va, _, _, import_name in self.vw.getImports():
+                for import_va, _, _, import_name in imports:
                     if import_name == callname:
                         cva = import_va
                         break
