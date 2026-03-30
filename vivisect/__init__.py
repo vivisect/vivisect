964560b5af5f2ddf274d55e6caf6bb302bb3274c
rakuyo <james.gross@vertex.link>
2026-03-30 11:35:32 -0400

    bump versions

--- vivisect/__init__.py
+++ vivisect/__init__.py
@@ -3373,6 +3373,6 @@ def getVivPath(*pathents):
 ##############################################################################
 # The following are touched during the release process by bump2version.
 # You should have no reason to modify these directly
-version = (1, 3, 0)
+version = (1, 3, 1)
 verstring = '.'.join([str(x) for x in version])
 commit = ''
